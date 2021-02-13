10001f70848e89df84c91892ac3fe61812a6ff58
danielalcaiden <100383530@alumnos.uc3m.es>
2021-02-13 19:08:38 +0100

    Primer commit

---
+++ app/src/main/java/es/uc3m/g1/musey/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.uc3m.g1.musey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
