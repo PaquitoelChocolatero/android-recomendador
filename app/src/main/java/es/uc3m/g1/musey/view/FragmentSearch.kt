d70e3a32ef96e9b1bf2f3b3b3df3a54794d4e96c
Alejandro de la Cruz <alexdelacruzalv@gmail.com>
2021-03-09 18:29:50 +0100

    Cosas de navigation

--- app/src/main/java/es/uc3m/g1/musey/view/FragmentSearch.kt
+++ app/src/main/java/es/uc3m/g1/musey/view/FragmentSearch.kt
@@ -1,20 +1,22 @@
 package es.uc3m.g1.musey.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import es.uc3m.g1.musey.R
 import es.uc3m.g1.musey.databinding.FragmentSearchBinding
-import es.uc3m.g1.musey.model.api.lastfm.Track
 import es.uc3m.g1.musey.viewModel.ViewModelSearch
+
 
 class FragmentSearch: Fragment() {
 
@@ -25,6 +27,12 @@ class FragmentSearch: Fragment() {
         super.onCreate(savedInstanceState)
         viewModelSearch = ViewModelProvider(this).get(ViewModelSearch::class.java)
     }
+
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,7 +60,18 @@ class FragmentSearch: Fragment() {
         }
         binding.button.setOnClickListener {
             viewModelSearch.search = binding.input.text.toString()
+            view?.hideKeyboard()
         }
+
+        binding.input.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                viewModelSearch.search = binding.input.text.toString()
+                view?.hideKeyboard()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+
         return binding.root
     }
 }
--- app/src/main/java/es/uc3m/g1/musey/view/MainActivity.kt
+++ app/src/main/java/es/uc3m/g1/musey/view/MainActivity.kt
@@ -1,12 +1,29 @@
 package es.uc3m.g1.musey.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import es.uc3m.g1.musey.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        val navigation: NavigationView = findViewById(R.id.navigation_view)
+        navigation.setNavigationItemSelectedListener {menuItem ->
+            val title: Int
+            title = when (menuItem.getItemId()) {
+                R.id.search -> R.string.search
+                else -> throw IllegalArgumentException("Menu option not implemented!!")
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            return true
+        }*/
     }
 }
